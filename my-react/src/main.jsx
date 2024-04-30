43c00f5c1754e062be27d9f0e09b407aa644cb9c
fecrakib <rakibislam132575@gmail.com>
2024-04-30 10:14:52 +0600

    todo complete

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TodoApp from './todo.jsx'
+import TodoApp from './Todo.jsx'
 import App from './App.jsx'
 import './index.css'
 
