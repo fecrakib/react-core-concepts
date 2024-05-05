6331545eb30a0acb67085b4efd778081258482a4
fecrakib <rakibislam132575@gmail.com>
2024-05-05 16:34:43 +0600

    error-router complete

---
+++ react-router/src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root';
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path:'/about',
+    element:<div> This is the about page</div>
+  }
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+ <RouterProvider router={router} />
+  </React.StrictMode>,
+)
